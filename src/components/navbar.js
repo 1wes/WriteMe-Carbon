1183c988e326b59a4137de4ac2acb7c5600e2df8
1wes <okemwawes@gmail.com>
2023-07-03 13:53:10 +0300

    implement desktop navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './navbar.css';
+import { Link } from "react-router-dom";
+
+const Logo=()=>{
+
+    return(
+        <React.Fragment>
+            <Link to={`/`} className="logo-image">
+                <img src={require('../pen.png')} /> <p><span>W</span>rite<span>M</span>e</p>
+            </Link>
+        </React.Fragment>
+    )
+}
+
+const Navbar=()=>{
+
+    return(
+        <React.Fragment>
+            <nav className="section">
+                <div className="desktop-nav">
+                    <div className="logo">
+                        <Logo/>
+                    </div>
+                    <ul className="nav-menu"> 
+                        <li>About us</li>
+                        <li>Why us</li>
+                        <li>Disciplines</li>
+                        <li>Contact us</li>
+                    </ul>
+                </div>
+            </nav>
+        </React.Fragment>
+    )
+}
+export default Navbar;
