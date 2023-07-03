6f9889746e872e6329d5641724065ede73c59a71
1wes <okemwawes@gmail.com>
2023-07-03 15:16:51 +0300

    import home section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/navbar';
+import Home from './components/home';
 
 const App=()=>{
 
   return(
     <React.Fragment>
       <Navbar/>
+      <Home/>
     </React.Fragment>
   )
 }
