d8d7143578364d011065fb40c93e08190fab4d30
1wes <okemwawes@gmail.com>
2023-07-03 12:51:12 +0300

    import navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css';
+import Navbar from './components/navbar';
 
 const App=()=>{
 
   return(
     <React.Fragment>
-
+      <Navbar/>
     </React.Fragment>
   )
 }
