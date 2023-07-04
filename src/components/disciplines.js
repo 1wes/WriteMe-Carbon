67fe3652cab30a99b3f6c1eb926d805c94217435
1wes <okemwawes@gmail.com>
2023-07-04 13:09:55 +0300

    add more disciplines

--- src/components/disciplines.js
+++ src/components/disciplines.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './disciplines.css';
 import SectionHeader from "./heading";
-import { GiMicroscope, GiMoneyStack } from "react-icons/gi";
+import { GiBrain, GiMoneyStack, GiChurch, GiMusicalNotes } from "react-icons/gi";
 import { GoLaw } from 'react-icons/go';
 import { MdSportsFootball, MdBusinessCenter, MdOutlineScience } from'react-icons/md';
 import {GiHealthIncrease, GiGraduateCap} from 'react-icons/gi';
 import {ImLibrary} from 'react-icons/im';
 import {PiHeartbeat} from 'react-icons/pi';
 import {AiOutlineDesktop, AiOutlinePieChart} from 'react-icons/ai'
+import {BsPeopleFill} from 'react-icons/bs';
 
 
 let Sectors=({icon, name})=>{
@@ -32,8 +33,8 @@ const Disciplines=()=>{
                     <SectionHeader heading={`Disciplines`} tagline={`We offer writing services spanning a wide range of discplines.`}/>
 
                     <div className="disciplines-list">
-                        <Sectors name={`Biology`} icon={<GiMicroscope/>}/>
                         <Sectors name={`History`} icon={<ImLibrary/>}/>
+                        <Sectors name={`Psychology`} icon={<GiBrain/>}/>
                         <Sectors name={`Healthcare`} icon={<GiHealthIncrease/>} />
                         <Sectors name={`Lifestyle & Wellness`} icon={<PiHeartbeat/>}/>
                         <Sectors name={`Business`} icon={<MdBusinessCenter/>} />
@@ -44,6 +45,9 @@ const Disciplines=()=>{
                         <Sectors name={`Finance`} icon={<GiMoneyStack/>}/>
                         <Sectors name={`Sports`} icon={<MdSportsFootball/>}/>
                         <Sectors name={`Marketing`} icon={<AiOutlinePieChart/>}/>
+                        <Sectors name={`Sociology`} icon={<BsPeopleFill/>}/>
+                        <Sectors name={`Humanities`} icon={<GiChurch/>}/>
+                        <Sectors name={`Art`} icon={<GiMusicalNotes/>}/>
                     </div>
                 </div>
             </div>
