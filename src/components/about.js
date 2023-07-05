f8fb657081fd2afb5739b6b81526403c124d7ff4
1wes <okemwawes@gmail.com>
2023-07-05 22:48:57 +0300

    show mobile navbar in about page on mobile devices

--- src/components/about.js
+++ src/components/about.js
@@ -3,12 +3,14 @@ import './about.css';
 import Navbar from "./navbar";
 import Footer from "./footer";
 import Breadcrumbs from "./breadcrumb";
+import { MobileNavbar } from "./navbar";
 
 const About=()=>{
 
     return(
         <React.Fragment>
             <Navbar/>
+            <MobileNavbar/>
             <div className="section" id="about-section">
                 <div className="about">
                     <Breadcrumbs/>
