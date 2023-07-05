1554a050fd034820c5d6021112f262d1f4384224
1wes <okemwawes@gmail.com>
2023-07-05 15:01:08 +0300

    create about route

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './components/about';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}></Route>
+      <Route path='/about' element={<About/>} ></Route>
     </Routes>
   </BrowserRouter>
 );
