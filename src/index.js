72e14d2b6c1686b66b6726bbb1269ab5d0dff016
1wes <okemwawes@gmail.com>
2023-07-05 15:54:35 +0300

    use hash router to enable rerendering of pages on routes other than root

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import About from './components/about';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path='/' element={<App/>}></Route>
       <Route path='/about' element={<About/>} ></Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
