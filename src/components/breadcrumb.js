1da439a79683eeb2c3b954c93af1b1be2cd92dd1
1wes <okemwawes@gmail.com>
2023-07-18 14:29:52 +0300

    create breadcrumb for faq page

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -3,6 +3,24 @@ import './breadcrumb.css';
 import { Link, useLocation } from "react-router-dom";
 import {IoIosArrowForward} from 'react-icons/io';
 
+const FAQBreadcrumb=()=>{
+
+    const location=useLocation();
+
+    return(
+        <React.Fragment>
+            <div className="breadcrumbs">
+                <Link to={`/`} className={location.pathname=='/'?"active-breadcrumb":"inactive-breadcrumb"}>
+                    home
+                </Link><span className="arrow-right"><i><IoIosArrowForward/></i></span>
+                <Link to={``} className={location.pathname=='/faq'?"active-breadcrumb":"inactive-breadcrumb"}>
+                    faq
+                </Link>
+            </div>
+        </React.Fragment>
+    )
+}
+
 const Breadcrumbs=()=>{
 
     const location=useLocation();
@@ -16,8 +34,12 @@ const Breadcrumbs=()=>{
                 <Link to={``} className={location.pathname=='/about'?"active-breadcrumb":"inactive-breadcrumb"}>
                     about us
                 </Link>
+
             </div>
         </React.Fragment>
     )
+}
+export{
+    FAQBreadcrumb
 }
 export default Breadcrumbs;
