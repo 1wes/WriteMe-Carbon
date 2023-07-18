2836931cb4cefe70fe4a9c8c29b867faa462ac81
1wes <okemwawes@gmail.com>
2023-07-18 14:14:47 +0300

    route to faq page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import About from './components/about';
+import FAQ from './components/faq';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +12,7 @@ root.render(
     <Routes>
       <Route path='/' element={<App/>}></Route>
       <Route path='/about' element={<About/>} ></Route>
+      <Route path='/faq' element={<FAQ/>} />
     </Routes>
   </HashRouter>
 );
