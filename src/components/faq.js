37b7a0de5a0f86d303371054e89cd757c874da83
1wes <okemwawes@gmail.com>
2023-07-19 16:11:13 +0300

    add faq accordions

--- src/components/faq.js
+++ src/components/faq.js
@@ -5,10 +5,39 @@ import { MobileNavbar } from "./navbar";
 import { FAQBreadcrumb } from "./breadcrumb";
 import SectionHeader from "./heading";
 import Footer from "./footer";
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from "@mui/material";
+import { BiChevronDown } from 'react-icons/bi'
+
+const AccordionComponent=({question, answer, expanded, onChange})=>{
+
+    return(
+        <React.Fragment>
+            <div className="accordion">
+                <Accordion expanded={expanded} sx={{width:"100%"}}  onChange={onChange} >
+                    <AccordionSummary  expandIcon={<BiChevronDown className="faq-icon"/>}>
+                        <Typography sx={{fontWeight:600, color:"#000000cc", fontSize:"13px"}}>
+                            {question}
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails> 
+                        <Typography sx={{fontSize:"13px"}}>
+                           {answer}
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+        </React.Fragment>
+    )
+}
 
 const FAQ=()=>{
 
     const [searchQuery, setSearchQuery]=useState('');
+    const [expanded, setExpanded]=useState(false);
+
+    const closeAccordion=(panel)=>(event, isExpanded)=>{
+        setExpanded(isExpanded ? panel : false)
+    }
 
     const handleSubmit=(e)=>{
 
@@ -42,6 +71,33 @@ const FAQ=()=>{
                     </form>
                 </div>
                 <div className="faq">
+                    <SectionHeader heading={`Frequently Asked Questions`} />
+                    <div className="faq-cards">
+                        <div className="questions">
+                            <AccordionComponent expanded={expanded==='panel1'} onChange={closeAccordion('panel1')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel2'} onChange={closeAccordion('panel2')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel3'} onChange={closeAccordion('panel3')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel4'} onChange={closeAccordion('panel4')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel5'} onChange={closeAccordion('panel5')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel6'} onChange={closeAccordion('panel6')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel7'} onChange={closeAccordion('panel7')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel8'} onChange={closeAccordion('panel8')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel9'} onChange={closeAccordion('panel9')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel10'} onChange={closeAccordion('panel10')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                        </div>
+                        <div className="questions">
+                            <AccordionComponent expanded={expanded==='panel11'} onChange={closeAccordion('panel11')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel12'} onChange={closeAccordion('panel12')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel13'} onChange={closeAccordion('panel13')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel14'} onChange={closeAccordion('panel14')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel15'} onChange={closeAccordion('panel15')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel16'} onChange={closeAccordion('panel16')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel17'} onChange={closeAccordion('panel17')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel18'} onChange={closeAccordion('panel18')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel19'} onChange={closeAccordion('panel19')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                            <AccordionComponent expanded={expanded==='panel20'} onChange={closeAccordion('panel20')} question={`How do i make an order`} answer={`Just fill in the contact form and we'll be in touch`}/>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer/>
