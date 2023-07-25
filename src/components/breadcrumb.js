b61999b5577d03513acbcc7bc734e2cbb71edde7
1wes <okemwawes@gmail.com>
2023-07-25 13:22:05 +0300

    get id of blog post and pass title prop to be used to display title of current blog being read

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -21,6 +21,27 @@ const FAQBreadcrumb=()=>{
     )
 }
 
+const BlogBreadcrumb=({title})=>{
+
+    const location=useLocation();
+
+    const id=location.pathname.split("/");
+
+    return(
+        <React.Fragment>
+            <div className="breadcrumbs">
+                <Link to={`/`} className={location.pathname==='/'?"active-breadcrumb":"inactive-breadcrumb"}>
+                    home
+                </Link><span className="arrow-right"><i><IoIosArrowForward/></i></span>
+                <Link  to={`/`} className={location.pathname==='/'?"active-breadcrumb":"inactive-breadcrumb"}>blog</Link><span className="arrow-right"><i><IoIosArrowForward/></i></span>
+                <Link to={``} className={location.pathname===`/blog/${id[2]}`?"active-breadcrumb":"inactive-breadcrumb"}>
+                    {title}
+                </Link>
+            </div>
+        </React.Fragment>
+    )
+}
+
 const Breadcrumbs=()=>{
 
     const location=useLocation();
@@ -40,6 +61,7 @@ const Breadcrumbs=()=>{
     )
 }
 export{
-    FAQBreadcrumb
+    FAQBreadcrumb,
+    BlogBreadcrumb
 }
 export default Breadcrumbs;
