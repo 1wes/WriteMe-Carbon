4dbbc581708e5be42b104fcd44cf14e818ccbc81
1wes <okemwawes@gmail.com>
2023-08-02 12:49:31 +0300

    opt into future flags to enable easier migration to react router v7

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import About from './components/about';
 import FAQ from './components/faq';
 import Blogpost from './components/blogpost';
+import NotFound from './components/404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <HashRouter future={{v7_startTransition:true}} >
     <Routes>
       <Route path='/' element={<App/>}></Route>
       <Route path='/about' element={<About/>}></Route>
       <Route path='/faq' element={<FAQ/>}/>
       <Route path='/blog/:slug' element={<Blogpost/>}></Route>
+      <Route path='*' element={<NotFound/>} ></Route>
     </Routes>
   </HashRouter>
 );
