b2e6d91ae51162e4f33d1ab96ebee267c235c216
1wes <okemwawes@gmail.com>
2023-08-07 16:10:49 +0300

    clear unused functions

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './home.css'
 import { CtaButton } from "./services";
-import { goToSection } from "../utils.js/section";
 import { Link } from "react-router-dom";
 
 const Home=()=>{
