06200de02e23109d7e53c1cc5348039618e2c263
1wes <okemwawes@gmail.com>
2023-08-08 15:34:31 +0300

    create assignment submission form

--- src/components/create-order.js
+++ src/components/create-order.js
@@ -4,6 +4,8 @@ import Navbar,{ MobileNavbar } from "./navbar";
 import { OrderBreadcrumb } from "./breadcrumb";
 import Footer from "./footer";
 import { Link } from "react-router-dom";
+import SectionHeader from "./heading";
+import { CtaButton } from "./services";
 
 const StepsBreadcrumb=()=>{
 
@@ -14,6 +16,132 @@ const StepsBreadcrumb=()=>{
                 <div className="step-info"><Link className='breadcrumb-link' to={`#`}>Get a quote and pay</Link></div>
                 <div className="step-info">Receive your work</div>
             </div>
+        </React.Fragment>
+    )
+}
+
+const SubmissionForm=()=>{
+
+    const handleWheel=e=>{
+
+        e.target.blur();
+
+        e.stopPropagation();
+    }
+
+    const submitAssignment=(e=>{
+
+        e.preventDefault();
+
+        alert("pushed")
+    })
+
+    return(
+
+        <React.Fragment>
+            <form className="assignment-form" onSubmit={submitAssignment}>
+            <SectionHeader heading={`Assignment Requirements`} />
+                <div className="assignment-details">
+                <div className="input-group">
+                    <label className="required">
+                        Subject
+                    </label>
+                    <div>
+                        <select name="subject" required>
+                            <option disabled selected hidden></option>
+                            <option value="Engineering">Engineering</option>
+                            <option value="Engineering">Programming</option>
+                            <option value="Engineering">History</option>
+                            <option value="Engineering">Main</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="input-group">
+                    <label className="required">
+                        Grade Level
+                    </label>
+                    <div>
+                        <select name="grade-level" required>
+                            <option disabled selected hidden></option>
+                            <option value="K12" >K12</option>
+                            <option value="Undergraduate" >Undergraduate</option>
+                            <option value="Postgraduate" >Postgraduate</option>
+                            <option value="Doctorate" >Doctorate</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="input-group">
+                    <label>
+                        Attach file
+                    </label>
+                    <div>
+                        <input  type="file" placeholder="add file" ></input>
+                    </div>
+                </div>
+                <div className="input-group">
+                    <label className="required">
+                        Instructions
+                    </label>
+                    <div>
+                        <textarea required></textarea>
+                    </div>
+                </div>
+                <div className="input-group">
+                    <label className="required">
+                        Number of Pages/Words
+                    </label>
+                    <div>
+                        <input type="number" onWheel={handleWheel}></input>
+                    </div>
+                </div>
+
+                <div className="input-group">
+                    <label className="required">
+                        Expected Amount (USD)
+                    </label>
+                    <div>
+                        <input type="number" onWheel={handleWheel}></input>
+                    </div>
+                </div>
+
+                <div className="input-group">
+                    <label className="required">
+                        Deadline
+                    </label>
+                    <div>
+                        <input type="date"></input>
+                    </div>
+                </div>
+                </div>
+                <SectionHeader heading={`Personal Information`} />
+                <div className="personal-info">
+                    <div className="input-group">
+                        <label className="required">
+                            Name
+                        </label>
+                        <div>
+                            <input type="text" required></input>
+                        </div>
+                    </div>
+                    <div className="input-group">
+                        <label className="required">
+                            Email
+                        </label>
+                        <div>
+                            <input type="email" required></input>
+                        </div>
+                    </div>
+                    <div className="input-group">
+                        <label>
+                            Phone Number
+                        </label>
+                        <div>
+                            <input type="tel"></input>
+                        </div>
+                    </div>
+                </div>
+                <CtaButton type={`submit`} message={`Submit Assignment`} />
+            </form>
         </React.Fragment>
     )
 }
@@ -33,6 +161,7 @@ const CreateOrder=()=>{
                 <div className="create-order">
                     <OrderBreadcrumb/>
                     <StepsBreadcrumb/>
+                    <SubmissionForm/>
                 </div>
             </section>
             <Footer/>
