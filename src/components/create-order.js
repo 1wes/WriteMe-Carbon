640e7188af7fa60356f213d1dffbcab05f5dafa5
1wes <okemwawes@gmail.com>
2023-08-08 11:37:04 +0300

    create a steps breadcrumb

--- src/components/create-order.js
+++ src/components/create-order.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import './create-order.css';
 import Navbar,{ MobileNavbar } from "./navbar";
 import { OrderBreadcrumb } from "./breadcrumb";
+import Footer from "./footer";
+import { Link } from "react-router-dom";
+
+const StepsBreadcrumb=()=>{
+
+    return(
+        <React.Fragment>
+            <div className="steps-breadcrumb">
+                <div className="step-info">Submit assignment alongside accurate details</div>
+                <div className="step-info"><Link className='breadcrumb-link' to={`#`}>Get a quote and pay</Link></div>
+                <div className="step-info">Receive completed work </div>
+            </div>
+        </React.Fragment>
+    )
+}
 
 const CreateOrder=()=>{
+
+    useLayoutEffect(()=>{
+        window.scrollTo(0, 0);
+    },[]);
 
     return(
 
@@ -13,8 +32,10 @@ const CreateOrder=()=>{
             <section className="section" id="create-order-section">
                 <div className="create-order">
                     <OrderBreadcrumb/>
+                    <StepsBreadcrumb/>
                 </div>
             </section>
+            <Footer/>
         </React.Fragment>
     )
 }
