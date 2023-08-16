70e868b0099e7687bfc5036d7f74a607a8fd0c93
1wes <okemwawes@gmail.com>
2023-08-16 15:19:17 +0300

    control signup form using state

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,36 +1,210 @@
-import React , {useEffect} from "react";
+import React , {useReducer} from "react";
 import './signup.css';
 import axios from "axios";
-import SectionHeader from './heading';
 import Countries from '../utils.js/countries.json';
 import Footer from './footer';
-import {CtaButton} from './services'
+import {CtaButton} from './services';
+import { FormAlerts, Error, FormLegend } from "./create-order";
+
+const reducer=(state, action)=>{
+
+    switch(action.type){
+
+        case "newFirstName":{
+            return{
+                firstName:action.newFirstName,
+                lastName:state.lastName,
+                email:state.email,
+                dialCode:state.dialCode,
+                phoneNumber:state.phoneNumber,
+                password:state.password,
+                confirmPassword:state.confirmPassword
+            }
+        }
+
+        case "newLastName":{
+            return{
+                firstName:state.firstName,
+                lastName:action.newLastName,
+                email:state.email,
+                dialCode:state.dialCode,
+                phoneNumber:state.phoneNumber,
+                password:state.password,
+                confirmPassword:state.confirmPassword
+            }
+        }
+
+        case "newEmail":{
+            return{
+                firstName:state.firstName,
+                lastName:state.lastName,
+                email:action.newEmail,
+                dialCode:state.dialCode,
+                phoneNumber:state.phoneNumber,
+                password:state.password,
+                confirmPassword:state.confirmPassword
+            }
+        }
+
+        case "newDialCode":{
+            return{
+                firstName:state.firstName,
+                lastName:state.lastName,
+                email:state.email,
+                dialCode:action.newDialCode,
+                phoneNumber:state.phoneNumber,
+                password:state.password,
+                confirmPassword:state.confirmPassword
+            }
+        }
+
+        case "newPhoneNumber":{
+            return{
+                firstName:state.firstName,
+                lastName:state.lastName,
+                email:state.email,
+                dialCode:state.dialCode,
+                phoneNumber:action.newPhoneNumber,
+                password:state.password,
+                confirmPassword:state.confirmPassword
+            }
+        }
+
+        case "newPassword":{
+            return{
+                firstName:state.firstName,
+                lastName:state.lastName,
+                email:state.email,
+                dialCode:state.dialCode,
+                phoneNumber:state.phoneNumber,
+                password:action.newPassword,
+                confirmPassword:state.confirmPassword
+            }
+        }
+
+        case "newConfirmPassword":{
+            return{
+                firstName:state.firstName,
+                lastName:state.lastName,
+                email:state.email,
+                dialCode:state.dialCode,
+                phoneNumber:state.phoneNumber,
+                password:state.password,
+                confirmPassword:action.newConfirmPassword
+            }
+        }
+
+        default:
+    }
+}
 
 const RegistrationForm=()=>{
+    
+    const initialState={
+        firstName:"",
+        lastName:"",
+        email:"",
+        dialCode:"",
+        phoneNumber:"",
+        password:"",
+        confirmPassword:""
+    }
+
+    const [state, dispatch]=useReducer(reducer, initialState);
 
     let countryCode=Countries.map(code=>{
         return <option key={code.code} value={code.dial_code}>{` ${code.emoji} ${code.name} (${code.dial_code})`}</option>
     })
 
+    const handleWheel=(e)=>{
+
+        e.target.blur();
+
+        e.stopPropagation();
+    }
+
+    const handleFirstNameChange=(e)=>{
+
+        dispatch({
+            type:"newFirstName",
+            newFirstName:e.target.value
+        })
+    }
+
+    const handleLastNameChange=(e)=>{
+
+        dispatch({
+            type:"newLastName",
+            newLastName:e.target.value
+        })
+    }
+
+    const handleEmailChange=(e)=>{
+
+        dispatch({
+            type:"newEmail",
+            newEmail:e.target.value
+        })
+    }
+
+    const handleDialCodeChange=(e)=>{
+
+        dispatch({
+            type:"newDialCode",
+            newDialCode:e.target.value
+        })
+    }
+
+    const handlePhoneChange=(e)=>{
+
+        dispatch({
+            type:"newPhoneNumber",
+            newPhoneNumber:e.target.value
+        })
+    }
+
+    const handlePasswordChange=(e)=>{
+
+        dispatch({
+            type:"newPassword",
+            newPassword:e.target.value
+        })
+    }
+
+    const handleConfirmationChange=(e)=>{
+
+        dispatch({
+            type:"newConfirmPassword",
+            newConfirmPassword:e.target.value
+        })
+    }
+
+    const handleSubmit=(e)=>{
+
+        e.preventDefault();
+        
+        alert(JSON.stringify(state));
+    }
+
     return(
         <React.Fragment>
-            <form className="signup-form">
+            <form className="signup-form" onSubmit={handleSubmit}>
                 <div className="input-group">
                     <label className="required">First name</label>
                     <div>
-                        <input type="text" required></input>
+                        <input type="text" value={state.firstName} onChange={handleFirstNameChange}  required></input>
                     </div>
                 </div>
                 <div className="input-group">
                     <label className="required">Last name</label>
                     <div>
-                        <input type="text" required ></input>
+                        <input type="text" value={state.lastName} onChange={handleLastNameChange} required ></input>
                     </div>
                 </div>
                 <div className="input-group">
                     <label className="required">Email</label>
                     <div>
-                        <input type="email" required ></input>
+                        <input type="email" value={state.email} onChange={handleEmailChange} required ></input>
                     </div>
                 </div>
                 <div className="input-group">
@@ -38,26 +212,27 @@ const RegistrationForm=()=>{
                         Phone Number
                     </label>
                     <div className="code-and-phone">
-                        <select  className="code">
+                        <select value={state.dialCode} onChange={handleDialCodeChange} className="code">
                             <option value={``} hidden disabled >🇦🇫 Afghanistan</option>
                             {countryCode}
                         </select>
-                    <input className="phone-number" type="number"></input>
+                    <input className="phone-number" type="number" onChange={handlePhoneChange} onWheel={handleWheel} ></input>
                     </div>
                 </div>
                 <div className="input-group">
                     <label className="required">Password</label>
                     <div>
-                        <input type="password" required></input>
+                        <input type="password" value={state.password} onChange={handlePasswordChange}  required></input>
                     </div>
                 </div>
                 <div className="input-group">
                     <label className="required">Confirm password</label>
                     <div>
-                        <input type="password" required></input>
+                        <input type="password" value={state.confirmPassword} onChange={handleConfirmationChange} required></input>
                     </div>
                 </div>
-                <CtaButton message={`Sign Up`} />
+                <FormLegend/>
+                <CtaButton type={`submit`} message={`Sign Up`} />
             </form>
         </React.Fragment>
     )
@@ -73,7 +248,7 @@ const SignUp=()=>{
                     <div className="reg-text"></div>
                     <div className="reg-form">
                         <div className="form-wrapper">
-                            <SectionHeader heading={`Sign up to WriteMe.com`} />
+                            <h4 className="signup-header">Sign Up </h4>
                             <RegistrationForm/>
                         </div>
                     </div>
