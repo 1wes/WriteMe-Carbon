64df8aa49500da547c2fc0bb4f72eea286e1f6b9
1wes <okemwawes@gmail.com>
2023-08-22 17:57:59 +0300

    mount user profile route

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import NotFound from './components/404';
 import CreateOrder from './components/create-order';
 import SignUp from './components/signup';
 import Login from './components/login';
+import UserProfile from './components/profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,6 +23,7 @@ root.render(
       <Route path='/submit-assignment' element={<CreateOrder/>} ></Route>
       <Route path='register' element={<SignUp/>} ></Route>
       <Route path='/login' element={<Login/>}></Route>
+      <Route path='/profile' element={<UserProfile/>} ></Route>
       <Route path='*' element={<NotFound/>} ></Route>
     </Routes>
   </HashRouter>
