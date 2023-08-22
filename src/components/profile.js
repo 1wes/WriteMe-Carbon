1c6f5e197de99b9aaf459ea9a67358755468a7de
1wes <okemwawes@gmail.com>
2023-08-22 20:49:42 +0300

    initial profile page and style

---
+++ src/components/profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './profile.css';
+
+const UserProfile=()=>{
+
+    return(
+        <React.Fragment>
+            <section className='section' id='profile-section'>
+                <div className='profile'>
+                    Am the profile
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
+export default UserProfile;
