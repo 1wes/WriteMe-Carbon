61459c4076ef097ed448677cf0f1b0aa4fa95676
1wes <okemwawes@gmail.com>
2023-08-28 23:12:21 +0300

    add forgot password page for routing in production

---
+++ src/components/forgot-password.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './forgot-password.css';
+
+const ForgotPassword=()=>{
+
+    return (
+        <React.Fragment>
+            <section className='section' id='forgotpwd-section'>
+                <div className='forgot-password'>
+                    Forgot password
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
+export default ForgotPassword;
