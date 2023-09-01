6ebd7f43809587115ac8aa95c0e7b8e16203d2e0
1wes <okemwawes@gmail.com>
2023-09-01 22:30:25 +0300

    display username on navbar profile section

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import './dashboard.css';
+import useSWR from 'swr';
+import axios from '../utils.js/axios';
+import { FiUser } from 'react-icons/fi'
+import { BsChevronDown } from 'react-icons/bs';
+
+const fetcher=url=>axios.get(url).then(res=>res.data);
+
+const DashboardNavbar=({userName})=>{
+
+    return(
+        <Fragment>
+            <nav className='dashboard-nav'>
+                <div className='dash-nav-content'>
+                    <div className='nav-logo'>
+                        Holla
+                    </div>
+                    <ul className='profile-menu'>
+                        <li className='profile-section'>
+                            <span className='user-profile-icon'>
+                                <i><FiUser/></i>
+                                <span className='username'>{userName}</span>
+                            </span>
+                            <span className='dropdown-icon' >
+                                <i><BsChevronDown/></i>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </Fragment>
+    )
+}
 
 const Dashboard=()=>{
 
+    const [userDetails, setUserDetails]=useState();
+
+    const {data}=useSWR(`/api/user/user-details`, fetcher);
+
+    const userNames=data
+
+    useEffect(()=>{
+        setUserDetails(userNames);
+    },[userNames])
+
     return(
         <React.Fragment>
-            <section className='section' id='profile-section'>
-                <div className='profile'>
-                    Am the profile
+            <section className='section' id='dashboard-section'>
+                <DashboardNavbar userName={userDetails}/>
+                <div className='dashboard'>
+                    dashboard
                 </div>
             </section>
         </React.Fragment>
