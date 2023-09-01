9f19b4872af0f3b8f0213f69cc5d4d8f044ccd5f
1wes <okemwawes@gmail.com>
2023-09-01 12:11:39 +0300

    change filename from profile to dshboard

---
+++ src/components/dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './dashboard.css';
+
+const Dashboard=()=>{
+
+    return(
+        <React.Fragment>
+            <section className='section' id='profile-section'>
+                <div className='profile'>
+                    Am the profile
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
+export default Dashboard;
