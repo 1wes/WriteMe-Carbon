cd75eb37d5c6731faa256e4723a3b1ad0e4f7a80
1wes <okemwawes@gmail.com>
2023-09-02 13:54:38 +0300

    toggle display of dropdown profile menu

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,28 +4,47 @@ import useSWR from 'swr';
 import axios from '../utils.js/axios';
 import { FiUser } from 'react-icons/fi'
 import { BsChevronDown } from 'react-icons/bs';
+import { MdOutlineManageAccounts } from 'react-icons/md';
+import { TbLogout } from 'react-icons/tb';
+import { IoNotificationsOutline } from 'react-icons/io5';
 
 const fetcher=url=>axios.get(url).then(res=>res.data);
 
 const DashboardNavbar=({userName})=>{
+
+    const showDropdownMenu=()=>{
+
+        const dropdownMenu=document.getElementById("user-profile-dropdown");
+        const icon=document.getElementById("down-icon");
+
+        icon.classList.toggle("rotate-icon");
+        dropdownMenu.classList.toggle("show-profile-dropdown")
+    }
 
     return(
         <Fragment>
             <nav className='dashboard-nav'>
                 <div className='dash-nav-content'>
                     <div className='nav-logo'>
-                        Holla
+                        WriteMe
                     </div>
                     <ul className='profile-menu'>
-                        <li className='profile-section'>
+                        <li className='profile-section' onClick={showDropdownMenu}>
                             <span className='user-profile-icon'>
                                 <i><FiUser/></i>
                                 <span className='username'>{userName}</span>
                             </span>
                             <span className='dropdown-icon' >
-                                <i><BsChevronDown/></i>
+                                <i><BsChevronDown id='down-icon' /></i>
                             </span>
                         </li>
+                        <div className='profile-dropdown' id='user-profile-dropdown'>
+                            <ul>
+                                <li className='dropdown-item'><span className='item-icon'><i><MdOutlineManageAccounts/></i></span> Account settings</li>
+                                <li className='dropdown-item'><span className='item-icon' ><i><IoNotificationsOutline/></i></span>Notifications</li>
+                                <li className='dropdown-item'><span className='item-icon'><i><TbLogout/></i></span> Sign out</li>
+                            </ul>
+                        </div>
                     </ul>
                 </div>
             </nav>
@@ -50,7 +69,7 @@ const Dashboard=()=>{
             <section className='section' id='dashboard-section'>
                 <DashboardNavbar userName={userDetails}/>
                 <div className='dashboard'>
-                    dashboard
+
                 </div>
             </section>
         </React.Fragment>
