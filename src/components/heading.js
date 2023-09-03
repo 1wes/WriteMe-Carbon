c5717110585c83257936b8a5081119ef86008056
1wes <okemwawes@gmail.com>
2023-09-03 15:38:56 +0300

    pass id prop to section header component

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './heading.css';
 
-const SectionHeader=({heading, tagline})=>{
+const SectionHeader=({heading, tagline, id})=>{
 
     return(
         <React.Fragment>
             <div className="header">
-                <h2 className="heading">{heading}</h2>
+                <h2 className="heading" id={id}>{heading}</h2>
                 <p>{tagline}</p>
             </div>
         </React.Fragment>
