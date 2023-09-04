0deae3a88781827c0d760303cd49ad93ccfd0570
1wes <okemwawes@gmail.com>
2023-09-04 17:50:13 +0300

    change submission form to exportable component

--- src/components/create-order.js
+++ src/components/create-order.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from "react";
+import React from "react";
 import './create-order.css';
 import { CtaButton } from "./services";
 
@@ -29,140 +29,11 @@ const Error=({errorMessage, id})=>{
     )
 }
 
-const reducer=(state, action)=>{
 
-    switch(action.type){
 
-        case "newSubject":{
-            return{
-                subject:action.newSubject,
-                gradeLevel:state.gradeLevel,
-                file:state.file,
-                instructions:state.instructions,
-                pagesOrwords:state.pagesOrwords,
-                amount:state.amount,
-                deadline:state.deadline,
-                time:state.time,
-            }
-        }
+const SubmissionForm=({onSubmit, subjectValue, onSubjectChange, gradeValue, onGradeChange, onFileChange, instructionsValue, onInstructionChange,
+    pagesOrwordsValue, onPagesChange, amountValue, onAmountChange, deadlineValue, onDeadlineChange,timeValue, onTimeChange, deadlineErrorAlert })=>{
 
-        case "newGradeLevel":{
-            return{
-                gradeLevel:action.newGrade,
-                subject:state.subject,
-                file:state.file,
-                instructions:state.instructions,
-                pagesOrwords:state.pagesOrwords,
-                amount:state.amount,
-                deadline:state.deadline,
-                time:state.time,
-            }
-        }
-
-        case "newFile":{
-            return{
-                file:action.newFile,
-                gradeLevel:state.gradeLevel,
-                subject:state.subject,
-                instructions:state.instructions,
-                pagesOrwords:state.pagesOrwords,
-                amount:state.amount,
-                deadline:state.deadline,
-                time:state.time,
-            }
-        }
-
-        case "newInstructions":{
-            return{
-                instructions:action.newInstructions,
-                pagesOrwords:state.pagesOrwords,
-                amount:state.amount,
-                deadline:state.deadline,
-                time:state.time,
-                file:state.file,
-                gradeLevel:state.gradeLevel,
-                subject:state.subject
-            }
-        }
-
-        case "newPagesOrWords":{
-            return{
-                pagesOrwords:action.newPages,
-                amount:state.amount,
-                deadline:state.deadline,
-                time:state.time,
-                file:state.file,
-                gradeLevel:state.gradeLevel,
-                subject:state.subject,
-                instructions:state.instructions
-            }
-        }
-
-        case "newAmount":{
-            return{
-                amount:action.newAmount,
-                pagesOrwords:state.pagesOrwords,
-                deadline:state.deadline,
-                time:state.time,
-                file:state.file,
-                gradeLevel:state.gradeLevel,
-                subject:state.subject,
-                instructions:state.instructions
-            }
-        }
-
-        case "newDeadline":{
-            return{
-                deadline:action.newDeadline,
-                amount:state.amount,
-                pagesOrwords:state.pagesOrwords,
-                file:state.file,
-                gradeLevel:state.gradeLevel,
-                subject:state.subject,
-                instructions:state.instructions,
-                time:state.time,
-            }
-        }
-
-        case "newTime":{
-            return{
-                time:action.newTime,
-                deadline:state.deadline,
-                amount:state.amount,
-                pagesOrwords:state.pagesOrwords,
-                file:state.file,
-                gradeLevel:state.gradeLevel,
-                subject:state.subject,
-                instructions:state.instructions
-            }
-        }
-
-        default:
-    }
-
-}
-
-const SubmissionForm=()=>{
-
-    const initialState={
-        subject:"",
-        gradeLevel:"",
-        file:"",
-        instructions:"",
-        pagesOrwords:"",
-        amount:"",
-        deadline:"",
-        name:"",
-        email:"",
-        code:"",
-        phone:"",
-        time:""
-    }
-
-    const [error, setError]=useState(false);
-    const [DeadlineErrorMessage, setDeadlineErrorMessage]=useState('');
-
-    const [state, dispatch]=useReducer(reducer, initialState);
 
     const handleWheel=e=>{
 
@@ -171,134 +42,17 @@ const SubmissionForm=()=>{
         e.stopPropagation();
     }
 
-    const handleSubjectChange=(e)=>{
-
-        dispatch({
-            type:"newSubject",
-            newSubject:e.target.value
-        })
-    }
-
-    const handleGradeChange=(e)=>{
-
-        dispatch({
-            type:"newGradeLevel",
-            newGrade:e.target.value
-        })
-    }
-
-    const handleFileChange=(e)=>{
-
-        const fileName=e.target.files[0];
-
-        dispatch({
-            type:"newFile",
-            newFile:fileName
-        })
-    }
-
-    const handleInstructionChange=(e)=>{
-
-        dispatch({
-            type:"newInstructions",
-            newInstructions:e.target.value
-        })
-    }
-
-    const handlePagesChange=(e)=>{
-
-        dispatch({
-            type:"newPagesOrWords",
-            newPages:e.target.value
-        })
-    }
-
-    const handleAmountChange=(e)=>{
-
-        dispatch({
-            type:"newAmount",
-            newAmount:e.target.value
-        })
-    }
-
-    const handleDeadlineChange=(e)=>{
-
-        const checkDate=()=>{
-
-            const deadline=new Date(e.target.value).valueOf();
-
-            const currentDate=new Date().valueOf();
-
-            return deadline>currentDate;
-        }
-
-        const isValid=checkDate();
-
-        if(isValid){
-            setError(false);
-
-            dispatch({
-                type:"newDeadline",
-                newDeadline:e.target.value
-            })
-        }else{
-            setError(!error);
-            setDeadlineErrorMessage("deadline cannot be in the past !!");
-        }
-    }
-
-    const handleTimeChange=(e)=>{
-
-        dispatch({
-            type:"newTime",
-            newTime:e.target.value
-        })
-    }
-
-    let DeadlineErrorAlert;
-
-    if(error){
-        DeadlineErrorAlert=(
-            <Error errorMessage={DeadlineErrorMessage} />
-        )
-    } 
-
-    const submitAssignment=(e=>{
-
-        e.preventDefault();
-
-        let assignmentDetails= new FormData();
-
-        assignmentDetails.append("name", "Wesley");
-
-        for (var key in state){
-            assignmentDetails.append(key, state[key])
-        }
-
-        alert(JSON.stringify(state));
-        
-        // axios.post("url", assignmentDetails, {
-        //     headers:{
-        //         "Content-Type":"multipart/form-data"
-        //     }
-        // }).then(res=>{
-        //     console.log(res)
-        // }).catch(err=>{
-        //     console.log(err);
-        // });
-    })
-
     return(
 
         <React.Fragment>
-            <form className="assignment-form" onSubmit={submitAssignment}>
+            <form className="assignment-form" onSubmit={onSubmit}>
                 <div className="assignment-details">
                 <div className="input-group">
                     <label className="required">
                         Subject
                     </label>
                     <div>
-                        <select name="subject" value={state.subject} onChange={handleSubjectChange} required>
+                        <select name="subject" value={subjectValue} onChange={onSubjectChange} required>
                             <option disabled value={``} hidden></option>
                             <option value="History">History</option>
                             <option value="Sports">Sports</option>
@@ -327,9 +81,9 @@ const SubmissionForm=()=>{
                         Grade Level
                     </label>
                     <div>
-                        <select name="grade-level" value={state.gradeLevel} onChange={handleGradeChange} required>
+                        <select name="grade-level" value={gradeValue} onChange={onGradeChange} required>
                             <option disabled value={``} hidden></option>
-                            <option value="K-12" >School</option>
+                            <option value="School" >School</option>
                             <option value="College">College</option>
                             <option value="Undergraduate">Undergraduate</option>
                             <option value="Postgraduate">Postgraduate</option>
@@ -342,7 +96,7 @@ const SubmissionForm=()=>{
                         Attach file
                     </label>
                     <div>
-                        <input type="file" onChange={handleFileChange} placeholder="add file" multiple></input>
+                        <input type="file" onChange={onFileChange} placeholder="add file" multiple></input>
                     </div>
                     <FormAlerts message={`supports only common image, document, video, and audio formats.`} />
                 </div>
@@ -351,7 +105,7 @@ const SubmissionForm=()=>{
                         Instructions
                     </label>
                     <div>
-                        <textarea value={state.instructions} onChange={handleInstructionChange} required></textarea>
+                        <textarea value={instructionsValue} onChange={onInstructionChange} required></textarea>
                     </div>
                 </div>
                 <div className="input-group">
@@ -359,7 +113,7 @@ const SubmissionForm=()=>{
                         Number of Pages/Words
                     </label>
                     <div>
-                        <input type="number" value={state.pagesOrwords} onChange={handlePagesChange} onWheel={handleWheel}></input>
+                        <input type="number" value={pagesOrwordsValue} onChange={onPagesChange} onWheel={handleWheel}></input>
                     </div>
                 </div>
 
@@ -368,7 +122,7 @@ const SubmissionForm=()=>{
                         Expected Amount (USD)
                     </label>
                     <div>
-                        <input type="number" value={state.amount} onChange={handleAmountChange} onWheel={handleWheel}></input>
+                        <input type="number" value={amountValue} onChange={onAmountChange} onWheel={handleWheel}></input>
                     </div>
                 </div>
 
@@ -377,9 +131,9 @@ const SubmissionForm=()=>{
                         Deadline Date
                     </label>
                     <div>
-                        <input type="date" value={state.deadline} onChange={handleDeadlineChange} required></input>
+                        <input type="date" value={deadlineValue} onChange={onDeadlineChange} required></input>
                     </div>
-                    {DeadlineErrorAlert}
+                    {deadlineErrorAlert}
                 </div>
 
                 <div className="input-group">
@@ -387,12 +141,12 @@ const SubmissionForm=()=>{
                         Deadline Time
                     </label>
                     <div>
-                        <input type="time" value={state.time} onChange={handleTimeChange}  required></input>
+                        <input type="time" value={timeValue} onChange={onTimeChange}  required></input>
                     </div>
                 </div>
                 </div>
                 <FormLegend/>
-                <CtaButton type={`submit`} message={`Submit Assignment`} />
+                <CtaButton type={`submit`} message={`Submit Assignment`} id={`submit-assignment-button`} />
             </form>
         </React.Fragment>
     )
