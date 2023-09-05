3ab444d8ac7e1428489f895da6be338b11e1c2bf
1wes <okemwawes@gmail.com>
2023-09-06 01:06:09 +0300

    change assignment submission links to login page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import About from './components/about';
 import FAQ from './components/faq';
 import Blogpost from './components/blogpost';
 import NotFound from './components/404';
-import CreateOrder from './components/create-order';
 import SignUp from './components/signup';
 import Login from './components/login';
 import Dashboard from './components/dashboard';
@@ -21,7 +20,6 @@ root.render(
       <Route path='/about' element={<About/>}></Route>
       <Route path='/faq' element={<FAQ/>}/>
       <Route path='/blog/:slug' element={<Blogpost/>}></Route>
-      <Route path='/submit-assignment' element={<CreateOrder/>} ></Route>
       <Route path='register' element={<SignUp/>} ></Route>
       <Route path='/login' element={<Login/>}></Route>
       <Route path='/user-dashboard' element={<Dashboard/>} ></Route>
