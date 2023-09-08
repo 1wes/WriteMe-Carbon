e2429bef37a33f22267c781fcba52aa4be1b5f0d
1wes <okemwawes@gmail.com>
2023-09-08 16:54:58 +0300

    build components for recurring input, textarea, and select form elements

--- src/components/create-order.js
+++ src/components/create-order.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {Fragment} from "react";
 import './create-order.css';
 import { CtaButton } from "./services";
 
@@ -29,6 +29,51 @@ const Error=({errorMessage, id})=>{
     )
 }
 
+const FormControl=({labelClassName, children, label})=>{
+
+    return(
+        <Fragment>
+            <div className="input-group">
+                <label className={labelClassName}>
+                    {label}
+                </label>
+                {children}
+            </div>
+        </Fragment>
+    )
+}
+
+const Select=({name, value, onChange, children})=>{
+
+    return(
+        <Fragment>
+            <select name={name} value={value} onChange={onChange} required>
+                {children}
+            </select>
+        </Fragment>
+    )
+}
+
+const Input=({type, onChange, placeholder, onWheel, value})=>{
+
+    return(
+        <Fragment>
+            <div>
+                <input type={type} onChange={onChange} placeholder={placeholder} onWheel={onWheel} value={value} ></input>
+            </div>
+        </Fragment>
+    )
+}
+
+const TextArea=({value, onChange})=>{
+
+    return(
+        <div>
+            <textarea value={value} onChange={onChange} required></textarea>
+        </div>
+    )
+}
+
 
 
 const SubmissionForm=({onSubmit, subjectValue, onSubjectChange, gradeValue, onGradeChange, onFileChange, instructionsValue, onInstructionChange,
@@ -42,108 +87,89 @@ const SubmissionForm=({onSubmit, subjectValue, onSubjectChange, gradeValue, onGr
         e.stopPropagation();
     }
 
+    const subjectOptions=(
+        <Fragment>
+            <option disabled value={``} hidden></option>
+            <option value="History">History</option>
+            <option value="Sports">Sports</option>
+            <option value="Programming">Programming</option>
+            <option value="Engineering">Engineering</option>
+            <option value="Psychology">Psychology</option>
+            <option value="Healthcare">Healthcare</option>
+            <option value="Lifestyle and wellness">Lifestyle & Wellness </option>
+            <option value="Business">Business</option>
+            <option value="Law">Law</option>
+            <option value="Technology">Technology</option>
+            <option value="Science">Science</option>
+            <option value="Education">Education</option>
+            <option value="Finance">Finance</option>
+            <option value="Marketing">Marketing</option>
+            <option value="Sociology">Sociology</option>
+            <option value="Humanities">Humanities</option>
+            <option value="Language">Language</option>
+            <option value="Art">Art</option>
+            <option value="Other">Other</option>
+        </Fragment>
+    )
+
     return(
 
         <React.Fragment>
             <form className="assignment-form" encType="multipart/form-data" onSubmit={onSubmit}>
                 <div className="assignment-details">
-                <div className="input-group">
-                    <label className="required">
-                        Subject
-                    </label>
-                    <div>
-                        <select name="subject" value={subjectValue} onChange={onSubjectChange} required>
-                            <option disabled value={``} hidden></option>
-                            <option value="History">History</option>
-                            <option value="Sports">Sports</option>
-                            <option value="Programming">Programming</option>
-                            <option value="Engineering">Engineering</option>
-                            <option value="Psychology">Psychology</option>
-                            <option value="Healthcare">Healthcare</option>
-                            <option value="Lifestyle and wellness">Lifestyle & Wellness </option>
-                            <option value="Business">Business</option>
-                            <option value="Law">Law</option>
-                            <option value="Technology">Technology</option>
-                            <option value="Science">Science</option>
-                            <option value="Education">Education</option>
-                            <option value="Finance">Finance</option>
-                            <option value="Marketing">Marketing</option>
-                            <option value="Sociology">Sociology</option>
-                            <option value="Humanities">Humanities</option>
-                            <option value="Language">Language</option>
-                            <option value="Art">Art</option>
-                            <option value="Other">Other</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="input-group">
-                    <label className="required">
-                        Grade Level
-                    </label>
-                    <div>
-                        <select name="grade-level" value={gradeValue} onChange={onGradeChange} required>
+                    <FormControl label={`Subject`} labelClassName={`required`}>
+                        <Select name={`subject`} value={subjectValue} onChange={onSubjectChange}>
+                            {subjectOptions}
+                        </Select>
+                    </FormControl>
+                    <FormControl label={`Grade Level`} labelClassName={`required`}>
+                        <Select name={`grade-level`} value={gradeValue} onChange={onGradeChange}>
                             <option disabled value={``} hidden></option>
                             <option value="School" >School</option>
                             <option value="College">College</option>
                             <option value="Undergraduate">Undergraduate</option>
                             <option value="Postgraduate">Postgraduate</option>
                             <option value="Doctorate">Doctorate</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="input-group">
-                    <label>
-                        Attach file
-                    </label>
-                    <div>
-                        <input type="file" onChange={onFileChange} placeholder="add file" multiple></input>
-                    </div>
-                    <FormAlerts message={`supports only common image, document, video, and audio formats.`} />
-                </div>
-                <div className="input-group">
-                    <label className="required">
-                        Instructions
-                    </label>
-                    <div>
-                        <textarea value={instructionsValue} onChange={onInstructionChange} required></textarea>
-                    </div>
-                </div>
-                <div className="input-group">
-                    <label className="required">
-                        Number of Pages/Words
-                    </label>
-                    <div>
-                        <input type="number" value={pagesOrwordsValue} onChange={onPagesChange} onWheel={handleWheel}></input>
-                    </div>
-                </div>
-
-                <div className="input-group">
-                    <label className="required">
-                        Expected Amount (USD)
-                    </label>
-                    <div>
-                        <input type="number" value={amountValue} onChange={onAmountChange} onWheel={handleWheel}></input>
-                    </div>
-                </div>
-
-                <div className="input-group">
-                    <label className="required">
-                        Deadline Date
-                    </label>
-                    <div>
-                        <input type="date" value={deadlineValue} onChange={onDeadlineChange} required></input>
-                    </div>
-                    {deadlineErrorAlert}
-                </div>
-
-                <div className="input-group">
-                    <label className="required">
-                        Deadline Time
-                    </label>
-                    <div>
-                        <input type="time" value={timeValue} onChange={onTimeChange}  required></input>
-                    </div>
-                </div>
+                        </Select>
+                    </FormControl>
+                    <FormControl label={`Number of Sources`} labelClassName={`required`}>
+                        <Input type={`number`} onWheel={handleWheel}  />
+                    </FormControl>
+                    <FormControl label={`Reference Style`} labelClassName={`required`}>
+                        <Select name={`reference-style`}>
+                            <option disabled value={``} hidden></option>
+                            <option value={`APA-7th`}>APA-7th Edition</option>
+                            <option value={`APA-6th`}>APA-6th Edition</option>
+                            <option value={`MLA`}>MLA</option>
+                            <option value={`Chicago/Turabian`}>Chicago/Turabian</option>
+                            <option value={`Harvard`}>Harvard</option>
+                            <option value={`ASA`}>ASA</option>
+                            <option value={`Oscolar`}>Oscolar</option>
+                            <option value={`Strayer`}>Strayer</option>
+                            <option value={`Any`}>Any</option>
+                            <option value={`None`}>None</option>
+                        </Select>
+                    </FormControl>
+                    <FormControl label={`Attach File`}>
+                        <Input type={`file`} onChange={onFileChange} placeholder={`add file`} multiple/>
+                        <FormAlerts message={`supports only common image, document, video, and audio formats.`} />
+                    </FormControl>
+                    <FormControl label={`Instructions`} labelClassName={`required`}>
+                        <TextArea value={instructionsValue} onChange={onInstructionChange}/>
+                    </FormControl>
+                    <FormControl label={`Number of Pages/Words`} labelClassName={`required`}>
+                        <Input type={`number`} value={pagesOrwordsValue} onChange={onPagesChange} onWheel={handleWheel}/>
+                    </FormControl>
+                    <FormControl label={`Expected Amount (USD)`} labelClassName={`required`}>
+                        <Input type={`number`} value={amountValue} onChange={onAmountChange} onWheel={handleWheel} required/>
+                    </FormControl>
+                    <FormControl label={`Date Deadline`} labelClassName={`required`}>
+                        <Input type={`date`} value={deadlineValue} onChange={onDeadlineChange} required/>
+                        {deadlineErrorAlert}
+                    </FormControl>
+                    <FormControl label={`Time Deadline`} labelClassName={`required`}>
+                        <Input type={`time`} value={timeValue} onChange={onTimeChange} required/>
+                    </FormControl>
                 </div>
                 <FormLegend/>
                 <CtaButton type={`submit`} message={`Submit Assignment`} id={`submit-assignment-button`} />
