2ee5f618fbec8db011545c9b9eeb098c02732ff3
1wes <okemwawes@gmail.com>
2023-09-13 18:00:59 +0300

    build modal form component

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,30 +1,66 @@
 import React, {Fragment} from 'react';
 import './modal.css';
-import {BiCheck} from 'react-icons/bi'
+import {BiCheck} from 'react-icons/bi';
+import { FormControl, TextArea } from './create-order';
+import { CtaButton } from './services';
 
-const Modal=({onClick, mainMessage, supportingMessage})=>{
+const ModalWrapper=({children, className, id})=>{
 
-    return(
+    return (
         <Fragment>
-            <div className='modal-wrapper' id='modal-wrap'>
+            <div className={className} id={id}>
                 <div className='modal-contents'>
                     <div className='modal'>
-                        <div className='success-icon'>
-                            <i className='success'><BiCheck/></i>
-                        </div>
-                        <h2>
-                            {mainMessage}
-                        </h2>
-                        <p>
-                            {supportingMessage}
-                        </p>
-                        <button onClick={onClick}>
-                            OK
-                        </button>
+                        {children}
                     </div>
                 </div>
             </div>
         </Fragment>
     )
+}
+
+const ModalForm=({formLabel,value, onChange, message, onSubmit, id, closeModal})=>{
+
+    return(
+        <Fragment>
+            <ModalWrapper id={id} >
+                <form onSubmit={onSubmit}>
+                    <FormControl label={formLabel} labelClassName={`required`} >
+                        <TextArea value={value} onChange={onChange}>
+                        </TextArea>
+                    </FormControl>
+                    <div className='modal-form-btn'>
+                        <CtaButton type={`submit`} id={`form-modal-button`} message={message}/>
+                        <CtaButton type={`button`} id={`modal-cancel-btn`} message={`Cancel`} onClick={closeModal}/>
+                    </div>
+                </form>
+            </ModalWrapper>
+        </Fragment>
+    )
+}
+
+const Modal=({onClick, mainMessage, supportingMessage})=>{
+
+    return(
+        <Fragment>
+            <ModalWrapper className={`modal-wrapper`} id={`modal-wrap`}>
+                <div className='success-icon'>
+                    <i className='success'><BiCheck/></i>
+                </div>
+                <h2>
+                    {mainMessage}
+                </h2>
+                <p>
+                    {supportingMessage}
+                </p>
+                <button onClick={onClick}>
+                    OK
+                </button>
+            </ModalWrapper>
+        </Fragment>
+    )
+}
+export{
+    ModalForm
 }
 export default Modal;
