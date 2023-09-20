0f43dcf167091ad6562059fe9168adad1d882585
1wes <okemwawes@gmail.com>
2023-09-20 17:13:58 +0300

    add admin orders metrics

--- src/components/admin.js
+++ src/components/admin.js
@@ -5,6 +5,9 @@ import useSWR from 'swr';
 import axios from '../utils.js/axios';
 import { useNavigate } from 'react-router-dom';
 import checkToken from '../utils.js/check-token';
+import { BsFileEarmarkBarGraph, BsFileEarmarkCheck } from 'react-icons/bs';
+import { GiSandsOfTime } from 'react-icons/gi';
+import { ImCancelCircle } from 'react-icons/im';
 
 const fetcher=url=>axios.get(url).then(res=>res.data);
 
@@ -12,6 +15,11 @@ const Admin=()=>{
 
     const [adminName, setAdminName]=useState('');
     const [loggedIn, setLoggedIn]=useState(true);
+    const [totalOrders, settotalOrders]=useState(0);
+    const [activeOrders, setActiveOrders]=useState(0);
+    const [completedOrders, setCompletedOrders]=useState(0);
+    const [cancelledOrders, setCancelledOrders]=useState(0);
+    const [allOrders, setAllOrders]=useState([]);
 
     const navigate=useNavigate();
 
@@ -19,9 +27,16 @@ const Admin=()=>{
 
     useEffect(()=>{
         if(data){
-            let { name }=data;
 
-            setAdminName(name);
+            let { username, totalOrders, allActiveOrders, allCancelledOrders, allCompletedOrders, allOrders }=data;
+
+            setAdminName(username);
+            settotalOrders(totalOrders);
+            setActiveOrders(allActiveOrders);
+            setCompletedOrders(allCompletedOrders);
+            setCancelledOrders(allCancelledOrders);
+            setAllOrders(allOrders);
+
         }
 
         checkToken().then(res=>{
@@ -54,8 +69,19 @@ const Admin=()=>{
         <Fragment>
             <section className='section' id='admin-dashboard'>
                 <DashboardNavbar userName={name} onClick={logoutUser} />
-                <div className='admin'>
-
+                <div className='dashboard' id='admin-dashboard'>
+                    <section className='overview'>
+                        <DashSectionHeaders heading={`Overview`}/>
+                        <div className='overview-metrics'>
+                            <Metrics title={`All Orders`} icon={<BsFileEarmarkBarGraph/>} number={totalOrders} />
+                            <Metrics title={`Completed Orders`} icon={<BsFileEarmarkCheck/>} number={completedOrders}/>
+                            <Metrics title={`Active Orders`} icon={<GiSandsOfTime/>} number={activeOrders} />
+                            <Metrics title={`Cancelled Orders`} icon={<ImCancelCircle/>} number={cancelledOrders} />
+                        </div>
+                    </section>
+                    <section>
+                        
+                    </section>
                 </div>
             </section>
         </Fragment>
