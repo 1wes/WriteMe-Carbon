1a766bf5977140371bd91fd0da749e88e225282a
1wes <okemwawes@gmail.com>
2023-09-21 17:04:24 +0300

    display all orders

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,9 +1,9 @@
 import React, {Fragment, useEffect, useState} from 'react';
 import './admin.css';
-import { DashSectionHeaders, Metrics, DashboardNavbar, OrdersTable } from './dashboard';
+import { DashSectionHeaders, Metrics, DashboardNavbar, OrdersTable, Search, GenericCtaButton } from './dashboard';
 import useSWR from 'swr';
 import axios from '../utils.js/axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import checkToken from '../utils.js/check-token';
 import { BsFileEarmarkBarGraph, BsFileEarmarkCheck } from 'react-icons/bs';
 import { GiSandsOfTime } from 'react-icons/gi';
@@ -65,6 +65,21 @@ const Admin=()=>{
         name=adminName
     }
 
+    const tableRows=(
+        allOrders.map((order)=>{
+
+            return (
+                <tr key={order.order_id}>
+                    <td>{`Order-${order.order_id}`}</td>
+                    <td>{order.subject}</td>
+                    <td>{order.status}</td>
+                    <td>{order.date_deadline.split("T")[0]}</td>
+                    <td><Link className='link'><GenericCtaButton id={`order-link`} message={`View Order`} /></Link></td>
+                </tr>
+            )
+        })
+    )
+
     return(
         <Fragment>
             <section className='section' id='admin-dashboard'>
@@ -79,8 +94,14 @@ const Admin=()=>{
                             <Metrics title={`Cancelled Orders`} icon={<ImCancelCircle/>} number={cancelledOrders} />
                         </div>
                     </section>
-                    <section>
-                        
+                    <section className='all-orders'>
+                        <DashSectionHeaders heading={`All Orders`} />
+                        <div className='orders-wrapper'>
+                            <Search/>
+                            <OrdersTable>
+                                {tableRows}
+                            </OrdersTable>
+                        </div>
                     </section>
                 </div>
             </section>
