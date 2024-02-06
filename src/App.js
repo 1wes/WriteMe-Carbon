6b893e58a857a92913d559c7575814a264e8c425
1wes <okemwawes@gmail.com>
2024-02-06 14:05:09 +0300

    temporary data fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import Home from './components/home';
@@ -11,7 +11,31 @@ import { MobileNavbar } from './components/navbar';
 import Bloglist from './components/bloglist';
 import HowItWorks from './components/how-it-works';
 
-const App=()=>{
+const App = () => {
+  
+  useEffect(() => {
+    
+    const fetchData = async () => {
+      try {
+          const usersResponse = await fetch('https://sil-ta-api.onrender.com/api/users');
+          const jsonUsers = await usersResponse.json();
+
+          // Process the fetched data as needed
+          console.log(jsonUsers);
+      } catch (error) {
+          console.error('Error fetching data:', error);
+      }
+  };
+
+  // Fetch data initially
+  fetchData();
+
+  // Set up interval to fetch data every 14 minutes
+  const intervalId = setInterval(fetchData, 14 * 60 * 1000);
+
+  // Clean up the interval on component unmount
+  return () => clearInterval(intervalId);
+  },[])
 
   return(
     <React.Fragment>
