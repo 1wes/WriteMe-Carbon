04dd27acb46979babc8490de3d45b95729b0ddf1
1wes <okemwawes@gmail.com>
2024-02-20 18:50:13 +0300

    feat: wrap entire application in auth provider

--- src/index.jsx
+++ src/index.jsx
@@ -13,22 +13,25 @@ import Dashboard from './components/dashboard';
 import ForgotPassword from './components/forgot-password';
 import Admin from './components/admin';
 import ClientOrder from './components/order';
+import AuthContextProvider from './context/Auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter future={{v7_startTransition:true}} >
-    <Routes>
-      <Route path='/' element={<App/>}></Route>
-      <Route path='/about' element={<About/>}></Route>
-      <Route path='/faq' element={<FAQ/>}/>
-      <Route path='/blog/:slug' element={<Blogpost/>}></Route>
-      <Route path='register' element={<SignUp/>} ></Route>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/user-dashboard' element={<Dashboard/>} ></Route>
-      <Route path='/forgot-password' element={<ForgotPassword/>} ></Route>
-      <Route path='/admin-dashboard' element={<Admin/>} ></Route>
-      <Route path='/admin/:orderId' element={<ClientOrder/>} ></Route>
-      <Route path='*' element={<NotFound/>} ></Route>
-    </Routes>
-  </HashRouter>
+  <AuthContextProvider>
+      <HashRouter future={{v7_startTransition:true}} >
+        <Routes>
+        <Route path='/' element={<App/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/faq' element={<FAQ/>}/>
+        <Route path='/blog/:slug' element={<Blogpost/>}></Route>
+        <Route path='register' element={<SignUp/>} ></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/user-dashboard' element={<Dashboard/>} ></Route>
+        <Route path='/forgot-password' element={<ForgotPassword/>} ></Route>
+        <Route path='/admin-dashboard' element={<Admin/>} ></Route>
+        <Route path='/admin/:orderId' element={<ClientOrder/>} ></Route>
+        <Route path='*' element={<NotFound/>} ></Route>
+      </Routes>
+    </HashRouter>
+  </AuthContextProvider>
 );
