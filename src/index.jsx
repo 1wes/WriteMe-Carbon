06f156b240ce9b70bb7a8f7e34a86c8172501bce
1wes <okemwawes@gmail.com>
2024-04-12 13:34:25 +0300

    fix(index): wrap relevant components with auth provider to prevent auto-nav to login from info pages

--- src/index.jsx
+++ src/index.jsx
@@ -18,20 +18,18 @@ import AuthContextProvider from './context/Auth';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <HashRouter future={{v7_startTransition:true}} >
-      <AuthContextProvider>
         <Routes>
           <Route path='/' element={<App/>}></Route>
           <Route path='/about' element={<About/>}></Route>
           <Route path='/faq' element={<FAQ/>}/>
           <Route path='/blog/:slug' element={<Blogpost/>}></Route>
           <Route path='register' element={<SignUp/>} ></Route>
-          <Route path='/login' element={<Login/>}></Route>
-          <Route path='/user-dashboard' element={<Dashboard/>} ></Route>
+          <Route path='/login' element={<AuthContextProvider><Login/></AuthContextProvider>}></Route>
+          <Route path='/user-dashboard' element={<AuthContextProvider><Dashboard/></AuthContextProvider>} ></Route>
           <Route path='/forgot-password' element={<ForgotPassword/>} ></Route>
-          <Route path='/admin-dashboard' element={<Admin/>} ></Route>
+          <Route path='/admin-dashboard' element={<AuthContextProvider><Admin/></AuthContextProvider>} ></Route>
           <Route path='/admin/:orderId' element={<ClientOrder/>} ></Route>
           <Route path='*' element={<NotFound />} ></Route>
         </Routes>
-      </AuthContextProvider>
     </HashRouter>
 );
