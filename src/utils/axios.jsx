a3d14c41de4e518fb50e0abb3abc3773e7636b09
1wes <okemwawes@gmail.com>
2024-09-13 17:59:50 +0300

    feat(axios): access backend server via the internet

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000", 
+    baseURL:"https://j498q3sl-5000.inc1.devtunnels.ms", 
 })
 
 axiosInstance.interceptors.request.use(
